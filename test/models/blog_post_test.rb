3796dedc60e0f9d6463db69ce39b2bb12eb8f697
Dishant Sharma <shaggydishu@gmail.com>
2024-06-26 11:44:14 +0530

    model minitests added

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @blog_post = BlogPost.new(title: 'Test Title', content: 'Test Content', user: @user)
+  end
+
+  test "should be valid" do
+    assert @blog_post.valid?
+  end
+
+  test "should require a title" do
+    @blog_post.title = nil
+    assert_not @blog_post.valid?
+  end
+
+  test "should require content" do
+    @blog_post.content = nil
+    assert_not @blog_post.valid?
+  end
+
+  test "should belong to a user" do
+    assert_instance_of User, @blog_post.user
+  end
 end
