e0006d7e1a2ccc612a806c7246f18fa88a0adcbe
Dishant Sharma <shaggydishu@gmail.com>
2024-06-26 10:00:37 +0530

    bootstrap cdn added with basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   as :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
     post 'login', to: 'devise/sessions#create', as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "application#home"
 end
